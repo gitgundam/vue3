a305bf59ae03fdf4c6e752ae53e6284cf79fb855
gitgundam <335968838@qq.com>
2020-12-22 14:59:27 +0800

    初步完成tabs组件功能

--- src/components/TabsDoc.vue
+++ src/components/TabsDoc.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="Tabs">
     33
-    <Tabs>
+    <Tabs v-model:selected="originTab">
       <Tab title="导航1">内容1</Tab>
-      <Tab title="导航2">内容2</Tab>
+      <Tab title="导航222">内容22</Tab>
+      <Tab title="导航3">内容333</Tab>
+      <Tab title="导航4">内容4444</Tab>
     </Tabs>
   </div>
 </template>
 
 <script lang="ts">
   import Tabs from '../lib/Tabs.vue';
-  import Tab from '../lib/Tab.vue';
+  import Tab from '../lib/Tab.vue'
+  import {ref} from 'vue'
 
   export default {
     name: "TabsDoc",
-    components:{Tabs,Tab}
+    components:{Tabs,Tab},
+    setup(){
+      const originTab = ref('导航222')
+      return {originTab}
+    }
+
 }
 </script>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,30 +1,124 @@
 <template>
   <div class="h-tabs">
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(cop,index) in defaults" :is="cop" :key="index"/>
+    <div class="h-tabs-nav" ref="container">
+      <div v-for="(t,index) in titles"
+           :ref="el => { if (el) navItems[index] = el }"
+           @click="switchTab(t)"
+           :key="index"
+           class="h-tabs-nav-item"
+           :class="{selected:selected === t}"
+          >{{t}}</div>
+      <div class="h-tabs-indicator" ref="indicator"></div>
+    </div>
+    <div class="h-tabs-content">
+      <component v-for="(cop,index) in defaults"
+                 class="h-tabs-content-item"
+                 :class="{selected:cop.props.title === selected}" :is="cop" :key="index"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
   import Tab from './Tab.vue';
+  import {ref,onMounted,onUpdated} from 'vue'
 
   export default {
     name: "Tabs",
-    setup(props,context){
+    props:{
+      selected:{
+        type: String
+      }
+    },
+    setup(props,context) {
+      const navItems = ref<HTMLDivElement[]>([])
+      const indicator =ref<HTMLDivElement>(null)
+      const container = ref<HTMLDivElement>(null)
+
+      onMounted(()=>{
+        const divs =navItems.value
+        const result = divs.filter(div=>div.classList.contains('selected'))[0]
+        const {width} = result.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {left: left1} = container.value.getBoundingClientRect()
+        const {left: left2} = result.getBoundingClientRect()
+        const left = left1 - left2
+        indicator.value.style.left = -left + 'px'
+
+      })
+      onUpdated(()=>{
+        const divs =navItems.value
+        const result = divs.filter(div=>div.classList.contains('selected'))[0]
+        const {width} = result.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {left: left1} = container.value.getBoundingClientRect()
+        const {left: left2} = result.getBoundingClientRect()
+        const left = left1 - left2
+        indicator.value.style.left = -left + 'px'
+
+      })
+
+
       const defaults = context.slots.default()
-      defaults.forEach((tag)=>{
-        if(tag.type !== Tab){
+      defaults.forEach((tag) => {
+        if (tag.type !== Tab) {
           throw Error('Tabs标签的子元素不能为除Tab标签外的其他元素')
         }
       })
-      const titles = defaults.map((tag)=>{
+      const titles = defaults.map((tag) => {
         return tag.props.title
       })
-      return {defaults,titles}
+      const switchTab = (title: string) => {
+        context.emit('update:selected', title)
+      }
+      return {defaults, titles,navItems,switchTab,indicator,container}
     }
 }
 </script>
 
-<style scoped lang="scss">
+<style  lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+ .h-tabs{
+   .h-tabs-nav{
+     display: flex;
+     color: $color;
+     border-bottom: 1px solid $border-color;
+     position: relative;
+     .h-tabs-nav-item{
+       padding: 8px 0;
+       margin: 0 16px;
+       cursor: pointer;
+       position: relative;
+       &:hover{
+         color: #40a9ff;
+       }
+       &:first-child {
+         margin-left: 0;
+       }
+       &.selected {
+         color: $blue;
+       }
+     }
+   }
+   &-content {
+     padding: 8px 0;
+     &-item{
+       display: none;
+       &.selected{
+         display: block;
+       }
+     }
+   }
+ }
+.h-tabs-indicator{
+  position: absolute;
+  top: 37px;
+  left: 0;
+  height: 3px;
+  background: $blue;
+  transition: all .25s;
+}
 
 </style>
