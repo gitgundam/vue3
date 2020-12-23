5ed5b8c127c2a0a7919a8890b12d85dbfcf9456d
“gitgundam” <335968838@qq.com>
2020-12-23 16:57:17 +0800

    完成Demo组件的封装,组件真是太爽啦

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,17 +1,72 @@
 <template>
-  <div class="demo">
-    <h2>标题</h2>
-    
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <component :is="component"></component>
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre class="language-html" v-html=" Prism.highlight(
+              component.__sourceCode,
+              Prism.languages.javascript,
+              'javascript'
+            )"></pre>
+      </div>
+    </div>
 
-  </div>
 </template>
 
-<script>
+<script lang='ts'>
+import "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+const Prism = (window as any).Prism;
+import Button from "../lib/Button.vue";
+import Switch1 from "../components/Switch.copy.vue";
+
 export default {
+  props:{
+    component: {
+      type: Object
+    }
+  },
+  components: {
+    Button
+  },
+  name: 'Demo',
+  setup(props){
+        return {Prism,Switch1 };
+  }
 
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+  $border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, "Courier New", Courier, monospace;
+      margin: 0;
+    }
+  }
+}
 </style>
--- src/components/SwitchDoc.vue
+++ src/components/SwitchDoc.vue
@@ -1,61 +1,25 @@
 <template>
-  <div class="switch-doc markdown-body">
+  <div class="switch-doc">
     <h1>Switch 组件示例</h1>
-    <div class="demo">
-      <div class="demo-component">
-         <Switch1></Switch1>
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code" ><pre>{{Switch1.__sourceCode}}</pre></div>
-    </div>
+    <Demo :component="Switch1"></Demo>
   </div>
 </template>
 
 <script lang="ts">
-import Switch from "../lib/Switch.vue";
-import Button from "../lib/Button.vue";
 import Switch1 from "../components/Switch.copy.vue";
-console.log(Switch1.__sourceCode);
-
-
-import { ref } from "vue";
+import Demo from "../components/Demo.vue";
 
 export default {
   name: "SwitchDoc",
-  components: { Button,Switch1 },
-  setup(){
-    return {Switch1}
-  }
+  components: {
+    Demo
+  },
+  setup() {
+    return { Switch1 };
+  },
 };
 </script>
 
 <style scoped lang="scss">
-$border-color: #d9d9d9;
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-  > h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-  &-component {
-    padding: 16px;
-  }
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    > pre {
-      line-height: 1.1;
-      font-family: Consolas, "Courier New", Courier, monospace;
-      margin: 0;
-    }
-  }
-}
+
 </style>
